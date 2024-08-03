3031550503b70ddebffb078152684e360fffd1d8
Bouaabdellah <bouaabd10@gmail.com>
2024-08-03 14:01:08 +0100

    modify the footer, by making it at the bottom of page all time

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Pages } from './pages/pages';
 
 function App() {
   return (
-    <div className='font-montSerrat pt-6'>
+    <div className='font-montSerrat pt-6 pb-48 relative min-h-[100vh]'>
       <Pages />
     </div>
   );
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 
 export const Footer = () => {
     return (
-        <div className="text-white text-[1rem] md:text-xl font-normal text-center py-6 bg-[#313131] mt-20">
+        <div className="text-white text-[1rem] md:text-xl font-normal text-center py-6 bg-[#313131] absolute left-0 bottom-0 w-full">
             created with love by <span className="font-semibold capitalize text-[#f27121] mx-2">-beloued-</span> in 2024
         </div>
     )
